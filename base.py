8d383c6d8c9932d433d533fbc479fe8787c82838
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-10-26 22:37:03 +0000

    initial commit

---
+++ base.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import pandas as pd
+import os
+import time
+
+
+driver = webdriver.Chrome(os.getcwd() + "/chromedriver")
+driver.get('https://imdb.com')
+
+#   Dropdown Variable
+dropdown = driver.find_element_by_class_name('ipc-icon--arrow-drop-down')
+dropdown.click()
+time.sleep(1)
+
+#   Advanced Search Under All (Dropdown Menu)
+element = driver.find_element_by_link_text('Advanced Search')
+element.click()
+
+#   Get and Click on Advanced Title Search link
+advanced_title = driver.find_element_by_link_text('Advanced Title Search')
+advanced_title.click()
+
+#   Select feature film checkbox
